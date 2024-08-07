d6371f672e249f7467205f8c8214ea982a029632
Priyanshu <Priyanshu174sharma@gmail.com>
2024-08-07 21:12:31 +0530

    added new projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import Social_Media from "../../Assets/Projects/Social_Media.png";
+import Book_Heaven from "../../Assets/Projects/Book_Heaven.jpg";
+import Chat_App from "../../Assets/Projects/Chat_App.jpg";
 import Ecommerce_react from "../../Assets/Projects/Ecommerce_react.png";
 import Ecommerce from "../../Assets/Projects/Ecommerce.png";
 import Quiz_Java from "../../Assets/Projects/Quiz_Java.png";
@@ -24,9 +26,29 @@ function Projects() {
               imgPath={Social_Media}
               isBlog={false}
               title="Social-Media"
-              description="Developed a full-stack social media application using React.js, Node.js, Express, and MongoDB. Implemented JWT authentication, bcrypt password encryption, and CRUD operations for posts, likes, and friend management. Utilized React hooks for state management and multer for file uploads. Deployed on Vercel."
+              description="Developed a full-stack social media application using React.js, Node.js, Express, and MongoDB. Implemented JWT authentication, bcrypt password encryption, and CRUD operations for posts, likes, and friend management. Utilized React hooks for state management and multer for file uploads. Deployed on Vercel and render."
               ghLink="https://github.com/Priyanshu174/MERN-SocialMediaApp"
               demoLink="https://mern-social-media-e02ndypf6-priyanshus-projects-f4f4f618.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Book_Heaven}
+              isBlog={false}
+              title="Book-Heaven"
+              description="Developed a book store application with a full-stack setup using React.js, Node.js, Express, and MongoDB. Implemented role-based access control for admin features, including adding, removing, and editing books, managing orders, and viewing user activities. Enabled users to explore books, add them to cart and favorites, and complete purchases with a redirect to a successful order page. Integrated JWT authentication, bcrypt for password encryption, and state management with Redux."
+              ghLink="https://github.com/Priyanshu174/My-BookStore-frontend"
+              demoLink="https://my-book-store-frontend-7gmr7ld0a-priyanshus-projects-f4f4f618.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Chat_App}
+              isBlog={false}
+              title="Chat-App"
+              description="Developed a full-stack chat application using React.js, Node.js, Express, and MongoDB. Implemented JWT authentication, bcrypt for password encryption, and real-time communication with Socket.IO. Integrated file uploads with Multer, emoji picker functionality, and profile management. Deployed on Vercel and Render."
+              ghLink="https://github.com/Priyanshu174/Mern_Chat_App_Frontend"
+              demoLink="https://mern-chat-app-frontend-leut7u4z0-priyanshus-projects-f4f4f618.vercel.app/"
             />
           </Col>
 
